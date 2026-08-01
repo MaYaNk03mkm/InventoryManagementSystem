87fe9e6b20082503740db9d383a7c7888fbdf46b
Mayank Sharma <mayanksharma.feb03@gmail.com>
2026-08-01 06:16:16 +0530

    Added dashboard charts

--- Inventory_SYstem/obj/Debug/net8.0/Inventory_SYstem.AssemblyInfo.cs
+++ Inventory_SYstem/obj/Debug/net8.0/Inventory_SYstem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inventory_SYstem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+589a6897957b5cf56ca9412e9a876a2a4fa5343f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+661c173df7670a1a4611b6244db335c0254af673")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inventory_SYstem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inventory_SYstem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
