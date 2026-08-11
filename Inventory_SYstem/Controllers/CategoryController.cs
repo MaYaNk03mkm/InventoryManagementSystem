6a1f18361fb6fd7804cf02e680ad8b1f93277d92
Mayank Sharma <mayanksharma.feb03@gmail.com>
2026-08-11 21:36:38 +0530

    category done

--- Inventory_SYstem/Controllers/CategoryController.cs
+++ Inventory_SYstem/Controllers/CategoryController.cs
@@ -1,18 +1,86 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using Inventory_SYstem.Data;
+using Inventory_SYstem.Models;
 
 namespace Inventory_SYstem.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             if (HttpContext.Session.GetString("Username") == null)
-            {
                 return RedirectToAction("Login", "Account");
-            }
+
+            var categories = _context.Categories.ToList();
+
+            return View(categories);
+        }
+        public IActionResult Create()
+        {
+            if (HttpContext.Session.GetString("Username") == null)
+                return RedirectToAction("Login", "Account");
 
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (HttpContext.Session.GetString("Username") == null)
+                return RedirectToAction("Login", "Account");
+
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
+        }
+        public IActionResult Edit(int id)
+        {
+            var category = _context.Categories.Find(id);
+
+            if (category == null)
+                return NotFound();
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(category);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
+        }
+        public IActionResult Delete(int id)
+        {
+            var category = _context.Categories.Find(id);
+
+            if (category == null)
+                return NotFound();
+
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
--- Inventory_SYstem/Controllers/StockOutController.cs
+++ Inventory_SYstem/Controllers/StockOutController.cs
@@ -1,69 +1,69 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Http;
-using Inventory_SYstem.Data;
-using Inventory_SYstem.Models;
-using System.Linq;
+﻿    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Http;
+    using Inventory_SYstem.Data;
+    using Inventory_SYstem.Models;
+    using System.Linq;
 
-namespace Inventory_SYstem.Controllers
-{
-    public class StockOutController : Controller
+    namespace Inventory_SYstem.Controllers
     {
-        private readonly ApplicationDbContext _context;
-
-        public StockOutController(ApplicationDbContext context)
+        public class StockOutController : Controller
         {
-            _context = context;
-        }
+            private readonly ApplicationDbContext _context;
 
-        private bool IsLoggedIn()
-        {
-            return HttpContext.Session.GetString("Username") != null;
-        }
-
-        // GET
-        public IActionResult Index()
-        {
-            if (!IsLoggedIn())
-                return RedirectToAction("Login", "Account");
-
-            ViewBag.Products = _context.Products.ToList();
-
-            return View();
-        }
-        [HttpPost]
-        public IActionResult Index(StockOut stockOut)
-        {
-            if (!IsLoggedIn())
-                return RedirectToAction("Login", "Account");
-
-            if (ModelState.IsValid)
+            public StockOutController(ApplicationDbContext context)
             {
-                var product = _context.Products.Find(stockOut.ProductId);
-
-                if (product != null)
-                {
-                    // Check available stock
-                    if (product.Quantity < stockOut.Quantity)
-                    {
-                        ViewBag.Error = "Insufficient Stock!";
-                        ViewBag.Products = _context.Products.ToList();
-                        return View(stockOut);
-                    }
-
-                    // Reduce stock
-                    product.Quantity -= stockOut.Quantity;
-
-                    // Save Stock Out record
-                    _context.StockOuts.Add(stockOut);
-
-                    _context.SaveChanges();
-
-                    return RedirectToAction("Index");
-                }
+                _context = context;
             }
 
-            ViewBag.Products = _context.Products.ToList();
-            return View(stockOut);
+            private bool IsLoggedIn()
+            {
+                return HttpContext.Session.GetString("Username") != null;
+            }
+
+            // GET
+            public IActionResult Index()
+            {
+                if (!IsLoggedIn())
+                    return RedirectToAction("Login", "Account");
+
+                ViewBag.Products = _context.Products.ToList();
+
+                return View();
+            }
+            [HttpPost]
+            public IActionResult Index(StockOut stockOut)
+            {
+                if (!IsLoggedIn())
+                    return RedirectToAction("Login", "Account");
+
+                if (ModelState.IsValid)
+                {
+                    var product = _context.Products.Find(stockOut.ProductId);
+
+                    if (product != null)
+                    {
+                        // Check available stock
+                        if (product.Quantity < stockOut.Quantity)
+                        {
+                            ViewBag.Error = "Insufficient Stock!";
+                            ViewBag.Products = _context.Products.ToList();
+                            return View(stockOut);
+                        }
+
+                        // Reduce stock
+                        product.Quantity -= stockOut.Quantity;
+
+                        // Save Stock Out record
+                        _context.StockOuts.Add(stockOut);
+
+                        _context.SaveChanges();
+
+                        return RedirectToAction("Index");
+                    }
+                }
+
+                ViewBag.Products = _context.Products.ToList();
+                return View(stockOut);
+            }
         }
     }
-}
